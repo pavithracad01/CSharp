a1a4c09326c8e76b1acce2ffcb177728ccfc1174
pavithracad01 <pavithracad01@gmail.com>
2024-02-19 16:04:00 +0530

    csharp file added

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+// using System;
+
+// namespace Program.cs
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Console.WriteLine("Welcome to Dotnet ");   
+
+//       int myint=10;
+//       Console.WriteLine(myint);
+
+
+//       int myNum = 5;               // integer (whole number)
+//       double myDoubleNum = 5.99D;  // floating point number
+//       char myLetter = 'D';         // character
+//       bool myBool = true;          // boolean
+//       string myText = "Hello";     // string
+//       Console.WriteLine(myNum);
+//       Console.WriteLine(myDoubleNum);
+//       Console.WriteLine(myLetter);
+//       Console.WriteLine(myBool);               
+//       Console.WriteLine(myText);
+
+
+//        int x = 100 + 50;
+//       Console.WriteLine(x); 
+
+
+  // class Program
+  // {
+  //   static void Mymethod(string fname,int age){
+  //       Console.WriteLine(fname + "is" + age);
+      
+  //     }
+
+  //     static void Main(string[] args){
+        
+  //       Mymethod("pavithra",22);
+  //       Mymethod("Nancy",24);
+  //       Mymethod("Dinesh",24);
+  //       }
+
+  class Program
+  {
+
+            static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Max(5, 10));  
+      Console.WriteLine(Math.Sqrt(81));
+      Console.WriteLine(Math.Abs(-2.5));
+      Console.WriteLine(Math.Round(99.5));
+      Console.WriteLine(Math.Min(5, 10));
+
+     }
+     }
+  
+
+
+
+
+
+  //   }
+  //   }
+  // }
+
+
